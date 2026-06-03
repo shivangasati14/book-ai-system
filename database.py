e3c0eb8b3743aed6fd282ab5effb47dcaa1a3939
Shivang Asati <Shivangasati1@gmail.com>
2026-06-03 11:09:06 +0530

    Update database connection for Neon

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="bookai_db",
-    user="postgres",
-    password="admin123"
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+
+conn = psycopg2.connect(DATABASE_URL)
 
 cursor = conn.cursor()
 
